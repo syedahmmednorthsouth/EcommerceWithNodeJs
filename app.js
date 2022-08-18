cc88eab04e95a1ca31766a46294fa024fd05672a
syedahmmednorthsouth <syed.ahmmed@northsouth.edu>
2022-08-19 01:06:55 +0600

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = 
+
+const server = app.listen()
+
