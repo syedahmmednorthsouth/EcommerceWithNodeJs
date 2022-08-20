de619a59db8d228c2f39ffb74ff0fd26ef96a245
syedahmmednorthsouth <syed.ahmmed@northsouth.edu>
2022-08-20 16:44:04 +0600

    add db config

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const path = require("path");
 const cors = require("cors");
+const mongoose = require('mongoose');
+const connectDb = require("./config/db");
 
 const app = express();
 
-const PORT = 
+app.use(express.json());
 
-const server = app.listen()
+dotenv.config({ path: "./config/config.env" });
 
+connectDb();
+
+
+
+
+
+const PORT = 3000;
+
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+);
